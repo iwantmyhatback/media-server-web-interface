0219126b41dc9b07699c3b5dd4c36d61448d1262
Tristan Deitzer <tristandeitzer@gmail.com>
2020-12-26 19:15:03 -0500

    Cleaning up CSS browser defaults

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'normalize.css';
 import './styles.css';
 
 var mountNode = document.getElementById('app');
